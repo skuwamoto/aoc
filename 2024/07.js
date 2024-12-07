fc5d0882652f1996e18e202d8c7e04d6043e95e5
Sho Kuwamoto <sho@figma.com>
2024-12-06 21:45:26 -0800

    day 7

--- 2024/07.js
+++ 2024/07.js
@@ -5,33 +5,48 @@ let test = fs.readFileSync('./test07.txt', {encoding:'utf8', flag:'r'});
 let input = fs.readFileSync('./input07.txt', {encoding:'utf8', flag:'r'});
 
 function parse(lines) {
-    lines = lines.split('\n').map(x => x.split(''))
+    lines = lines.split('\n').map(x => { return {answer: Number(x.before(':')), terms: x.after(': ').split(' ').map(Number) }})
     return lines
 }
 
 test = parse(test)
 input = parse(input)
 
-console.log(test)
-console.log()
+function check(answer, first, rest, allowConcat) {
+    if (rest.length == 0) return answer == first
 
-// test = test.stringToGrid()
-// input = input.stringToGrid()
+    let next = rest.slice(1)
+    if (check(answer, first + rest[0], next, allowConcat)) return true
+    if (check(answer, first * rest[0], next, allowConcat)) return true
+    if (allowConcat && check(answer, Number('' + first + rest[0]), next, allowConcat)) return true
 
-// test.print()
+    return false
+}
 
 function partA(info) {
     let sum = 0
+
+    for (let {answer, terms} of info) {
+        if (check(answer, terms[0], terms.slice(1), false)) {
+            sum += answer
+        }
+    }
     return sum
 }
 
 function partB(info) {
     let sum = 0
+
+    for (let {answer, terms} of info) {
+        if (check(answer, terms[0], terms.slice(1), true)) {
+            sum += answer
+        }
+    }
     return sum
 }
 
 console.log(partA(test))
-// console.log(partA(input))
-// console.log('--')
-// console.log(partB(test))
-// console.log(partB(input))
+console.log(partA(input))
+console.log('--')
+console.log(partB(test))
+console.log(partB(input))
